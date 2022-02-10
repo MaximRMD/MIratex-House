3430dd0817b3c4e172ec269706866338662d402d
Maxim <maxRMD@yandex.ru>
2022-02-10 23:13:57 +0300

    сборка gulp ES6 modules

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -1,22 +1,31 @@
-import * as nodePath from 'path';
+import * as nodePath from "path";
 const rootFolder = nodePath.basename(nodePath.resolve());
 
-const buildFolder = './dist';
-const srcFolder = './src';
+const buildFolder = "./dist";
+const srcFolder = "./src";
 
 export const path = {
     build: {
-        files: '${buildFolder}/files/',
+        js: `${buildFolder}/js/`,
+        css: `${buildFolder}/css/`,
+        html: `${buildFolder}/`,
+        files: `${buildFolder}/files/`,
     },
     src: {
-        files: '${srcFolder}/files/**/*.*',
+        js: `${srcFolder}/js/app.js`,
+        scss: `${srcFolder}/scss/style.scss`,
+        html: `${srcFolder}/*.html`,
+        files: `${srcFolder}/files/**/*.*`,
     },
     watch: {
-        files: '${srcFolder}/files/**/*.*',
+        js: `${srcFolder}/js/**/*.js`,
+        scss: `${srcFolder}/scss/**/*.scss`,
+        html: `${srcFolder}/**/*.html`,
+        files: `${srcFolder}/files/**/*.*`,
     },
     clean: buildFolder,
     buildFolder: buildFolder,
     srcFolder: srcFolder,
     rootFolder: rootFolder,
-    ftp: ''
-}
+    ftp: "",
+};
